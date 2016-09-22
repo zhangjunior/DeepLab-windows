60c44737f014d3a9ec19bd1fbdeecfb4c71ecb3d
zhangjunior <pluszhang@outlook.com>
2016-09-22 16:37:50 +0800

    deeplab windows

---
+++ include/caffe/util/matio_io.hpp
@@ -0,0 +1,30 @@
+#ifndef CAFFE_UTIL_MATIO_IO_H_
+#define CAFFE_UTIL_MATIO_IO_H_
+
+#ifdef  _WIN32
+#include <io.h>
+#include <process.h>
+#else
+#include <unistd.h>
+#endif
+
+#include <string>
+#include "google/protobuf/message.h"
+#include "caffe/blob.hpp"
+#include "caffe/common.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+using ::google::protobuf::Message;
+
+template <typename Dtype>
+void ReadBlobFromMat(const char *fname, Blob<Dtype>* blob);
+
+template <typename Dtype>
+void WriteBlobToMat(const char *fname, bool write_diff,
+   Blob<Dtype>* blob);
+
+}  // namespace caffe
+
+#endif   // CAFFE_UTIL_IO_H_
